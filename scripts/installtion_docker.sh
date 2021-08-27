73f90c46b2d83299c539d5b887aa073e38747540
hossameltohamy <hossamyahia1017@gmail.com>
2021-08-27 13:33:39 +0000

    sf

--- scripts/installtion_docker.sh
+++ scripts/installtion_docker.sh
@@ -26,13 +26,17 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 echo "Docker Compose Installed"
 
+
 # install jenkins 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+   # install java 
+   sudo apt install openjdk-11-jdk
+   java -version
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9B7D32F2D50582E6
+systemctl status jenkins
 sudo ufw allow 8080
 sudo ufw allow OpenSSH
 sudo ufw enable
