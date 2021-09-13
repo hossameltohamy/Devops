c76a073cb4db425d1a4c623c00ec0ff34023ea57
hossam yahia <hossam@dafholding.com>
2021-09-13 21:28:47 +0200

    fs

---
+++ gitlab-cicd/ec2-prepration-script.sh
@@ -0,0 +1,31 @@
+#gitlab runner 
+# For Debian/Ubuntu/Mint
+curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+sudo apt-get install gitlab-runner
+# node app
+  sudo apt install nodejs
+  sudo apt install npm
+
+# docker 
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+cd /var/run/ && sudo chmod 777 docker.pid docker.sock
+sudo apt-get install zip
+sudo mkdir -p /var/lib/gitlab-runner
+sudo gitlab-runner start 
